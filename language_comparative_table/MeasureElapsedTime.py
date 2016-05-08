d162e06110da4cb84863e39d40193001145cf5a4
matsu7874 <mtsmtkmt@gmail.com>
2016-05-08 21:47:09 +0900

    update functionized

--- language_comparative_table/MeasureElapsedTime.py
+++ language_comparative_table/MeasureElapsedTime.py
@@ -2,12 +2,19 @@ import time
 
 def DoSomething():
     a = 0
-    for i in range(1000000000):
+    for i in range(100000):
         a += 1
+    return a
 
-START_TIME = time.time()
+def measure_elapsed_time(func):
+    START_TIME = time.time()
 
-DoSomething()
+    res = func()
 
-ELAPSED_TIME = time.time() - START_TIME
-print('Elapsed Time:', ELAPSED_TIME*1000 ,'[ms]')
+    ELAPSED_TIME = time.time() - START_TIME
+
+    print('Elapsed Time:', ELAPSED_TIME*1000 ,'[ms]')
+    print(res)
+
+if __name__ == '__main__':
+    measure_elapsed_time(DoSomething)
