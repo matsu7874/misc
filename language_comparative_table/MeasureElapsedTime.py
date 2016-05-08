512746efa9c1a4f97aedecc8e9202e4228af5dc9
matsu7874 <mtsmtkmt@gmail.com>
2016-05-08 22:16:01 +0900

    refactor

--- language_comparative_table/MeasureElapsedTime.py
+++ language_comparative_table/MeasureElapsedTime.py
@@ -1,19 +1,19 @@
-import time
-
 def DoSomething():
     a = 0
     for i in range(100000):
         a += 1
     return a
 
+
 def measure_elapsed_time(func):
+    import time
     START_TIME = time.time()
 
     res = func()
 
     ELAPSED_TIME = time.time() - START_TIME
 
-    print('Elapsed Time:', ELAPSED_TIME*1000 ,'[ms]')
+    print('Elapsed Time:', ELAPSED_TIME * 1000, '[ms]')
     print(res)
 
 if __name__ == '__main__':
